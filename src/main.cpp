02d291e09d29c8538239e95886f5ad2f8afe652c
Aleph-Design <jan@aleph-design.nl>
2021-07-24 14:38:59 +0200

    Finally initial version running

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,8 @@
 #include <Arduino.h>
 
 static void taskOneHandler(void *pvParam);
-/*
 static void taskTwoHandler(void *pvParam);
-*/
+
 
 void setup() 
 {
@@ -19,35 +18,48 @@ void setup()
   Serial.printf("configMAX_PRIORITIES: %d\n\n", configMAX_PRIORITIES);
 
 
-  TaskHandle_t taskOneHandle = NULL;   // actually a pointer
-  //TaskHandle_t taskTwoHandle = NULL;
+  TaskHandle_t taskOneHandle = NULL;   // actually just a pointer
+  TaskHandle_t taskTwoHandle = NULL;
 
   BaseType_t status;                   // 32-bits variable
 
   const char* textOne = "1111111111111111111111111111111111111";
-  //const char* textTwo = "2222222222222222222222222222222222222";
+  const char* textTwo = "2222222222222222222222222222222222222";
 
-  status = xTaskCreate(
+  /*
+  * Using core 0 runs into:
+  * E (11183) task_wdt: Task watchdog got triggered. 
+  * The following tasks did not reset the watchdog in time:
+  * E (11183) task_wdt:  - IDLE0 (CPU 0)
+  * E (11183) task_wdt: Tasks currently running:
+  * E (11183) task_wdt: CPU 0: Task-1
+  * E (11183) task_wdt: CPU 1: IDLE1
+  * E (11183) task_wdt: Aborting.
+  * abort() was called at PC 0x400e111f on core 0
+  */
+  status = xTaskCreatePinnedToCore(
               taskOneHandler, 
               "Task-1", 
-              2048,
+              1536,
               (void*)textOne,
               2,
-              &taskOneHandle);
-              //1);
+              &taskOneHandle,
+              1);
 
   configASSERT(status = pdPASS);  // if evaluate to 1, no activation
 
-/*
-  status = xTaskCreate(taskTwoHandler, 
+
+  status = xTaskCreatePinnedToCore(taskTwoHandler, 
               "Task-2", 
-              1024,
+              1536,
               (void*)textTwo,
               2,
-              &taskTwoHandle);
+              &taskTwoHandle,
+              1);
 
   configASSERT(status = pdPASS);  // if evaluate to 1, no activation
-*/
+
+  //vTaskDelete(NULL);    // Delete loop()
 
 } // end setup() -------------------------------------------------------------
 
@@ -61,12 +73,13 @@ void loop() {
 //
 static void taskOneHandler(void *pvParam)
 {
-  //const char* localTextOne = (char*) pvParam;
-  //const char* localTextOne = "1111111111111111111111111111111111111";
-
   for (;;)
   {
-    Serial.printf("localTextOne: %s\n", (char*) pvParam);
+    Serial.printf("%s\n", (char*) pvParam);
+
+    // Necessary to give task-2 a chance
+    //
+    vTaskDelay(1 / portTICK_PERIOD_MS);
   }
 
   vTaskDelete(NULL);
@@ -74,17 +87,14 @@ static void taskOneHandler(void *pvParam)
 } // end taskOneHandler()
 
 
-/*
+
 static void taskTwoHandler(void *pvParam)
 {
-  const char* localTextTwo = (char*) pvParam;
-
   for (;;)
   {
-    Serial.printf("localTextTwo: %s", localTextTwo);
+    Serial.printf("%s\n", (char*) pvParam);
   }
 
   vTaskDelete(NULL);
   
 } // end taskTwoHandler()
-*/
